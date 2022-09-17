b15100b4861bc86bb5d365da8ceb872ef13aa4c5
Hector Gonzalez <gorozco.hector@gmail.com>
2022-09-16 20:13:12 -0400

    Added order and balance methods to Tree class

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -8,9 +8,16 @@ class Node {
 
 class Tree {
   #root;
+  #queue = [this.#root];
+  #nodesArray = [];
   constructor(array = []) {
     // Makes tree right away with unput array
     this.array = this.buildTree(array);
+  }
+
+  // This is used as the default callback function by levelOrder
+  #addToNodesArray(node) {
+    this.#nodesArray.push(node.value);
   }
 
   #removeDuplicates(array) {
@@ -106,8 +113,8 @@ class Tree {
   }
 
   delete(value, node = this.#root) {
-    if(node === null){
-        console.log(`No ${value} exists in tree`);
+    if (node === null) {
+      console.log(`No ${value} exists in tree`);
     }
     let nodeToDelete;
     let side;
@@ -115,12 +122,12 @@ class Tree {
     // value is on left
     if (node.leftChild != null && node.leftChild.value === value) {
       nodeToDelete = node.leftChild;
-      side = 'leftChild'
+      side = "leftChild";
     } else if (node.rightChild != null && node.rightChild.value === value) {
       nodeToDelete = node.rightChild;
-      side = 'rightChild';
-    } else if(node.value === value){
-        nodeToDelete = node
+      side = "rightChild";
+    } else if (node.value === value) {
+      nodeToDelete = node;
     }
 
     // If value is either in the left or rightside
@@ -137,58 +144,60 @@ class Tree {
       if (nodeToDelete.leftChild !== null && nodeToDelete.rightChild === null) {
         node[side] = nodeToDelete.leftChild;
         return;
-      } else if (nodeToDelete.rightChild !== null && nodeToDelete.leftChild === null) {
+      } else if (
+        nodeToDelete.rightChild !== null &&
+        nodeToDelete.leftChild === null
+      ) {
         node[side] = nodeToDelete.rightChild;
         return;
       }
 
       // Case 3
       // If it has two children
-        // Find the next biggest item, it's in the leftmost position of its right subtree
-    let nodeBeforeBiggest;
-    let nextBiggest = nodeToDelete.rightChild;
-    while(nextBiggest.leftChild !== null){
-        nodeBeforeBiggest = nextBiggest
-        nextBiggest = nextBiggest.leftChild
-    }
+      // Find the next biggest item, it's in the leftmost position of its right subtree
+      let nodeBeforeBiggest;
+      let nextBiggest = nodeToDelete.rightChild;
+      while (nextBiggest.leftChild !== null) {
+        nodeBeforeBiggest = nextBiggest;
+        nextBiggest = nextBiggest.leftChild;
+      }
 
-        // If it has no children, 
-        if(nextBiggest.rightChild === null){
-            
-          //switch it with the original
-          const original = {...nodeToDelete}
-          
-          if(nodeToDelete === this.#root){
-            this.#root = {...nextBiggest};
-            this.#root.rightChild = original.rightChild;
-            this.#root.leftChild = original.leftChild;
+      // If it has no children,
+      if (nextBiggest.rightChild === null) {
+        //switch it with the original
+        const original = { ...nodeToDelete };
+
+        if (nodeToDelete === this.#root) {
+          this.#root = { ...nextBiggest };
+          this.#root.rightChild = original.rightChild;
+          this.#root.leftChild = original.leftChild;
         } else {
-          node[side] = {...nextBiggest};
+          node[side] = { ...nextBiggest };
           node[side].rightChild = original.rightChild;
-          node[side].leftChild = original.leftChild
-        }
-          
-          // Add null instead
-          nodeBeforeBiggest.leftChild = null
-          return
+          node[side].leftChild = original.leftChild;
         }
 
-        // It it has a right subtree
-          //switch it with the original
-          const original = nodeToDelete
-          
-          if(nodeToDelete === this.#root){
-            this.#root = {...nextBiggest};
-            this.#root.rightChild = original.rightChild;
-            this.#root.leftChild = original.leftChild;
-        } else {
-          node[side] = {...nextBiggest};
-          node[side].rightChild = original.rightChild;
-          node[side].leftChild = original.leftChild
-        }
-          // Add children instead instead
-          nodeBeforeBiggest.leftChild= nextBiggest.rightChild;
-          return
+        // Add null instead
+        nodeBeforeBiggest.leftChild = null;
+        return;
+      }
+
+      // It it has a right subtree
+      //switch it with the original
+      const original = nodeToDelete;
+
+      if (nodeToDelete === this.#root) {
+        this.#root = { ...nextBiggest };
+        this.#root.rightChild = original.rightChild;
+        this.#root.leftChild = original.leftChild;
+      } else {
+        node[side] = { ...nextBiggest };
+        node[side].rightChild = original.rightChild;
+        node[side].leftChild = original.leftChild;
+      }
+      // Add children instead instead
+      nodeBeforeBiggest.leftChild = nextBiggest.rightChild;
+      return;
     }
 
     // Recursive case
@@ -197,6 +206,173 @@ class Tree {
     } else if (node.value < value) {
       this.delete(value, node.rightChild);
     }
+  }
+
+  // Runs callback function with every node in tree, in breadth-first order
+  // If no callback if provided, add every node to array
+  // and then print the result
+  levelOrder(callback = this.#addToNodesArray, node = this.#root) {
+    // Enqueue both children
+    if (node.leftChild !== null) {
+      this.#queue.push(node.leftChild);
+    }
+    if (node.rightChild !== null) {
+      this.#queue.push(node.rightChild);
+    }
+
+    // Dequeue self
+    this.#queue.shift();
+
+    callback.call(this, node);
+
+    // Base case
+    if (this.#queue.length < 1) {
+      // Restart queue
+      this.#queue.push(this.#root);
+      // Print array with Nodes if not provided with callback function
+      if (callback === this.#addToNodesArray) {
+        const arrayCopy = [...this.#nodesArray]
+        this.#nodesArray = []
+        return arrayCopy
+      }
+    } else {
+      // if theres anything in the queue, call it
+      return this.levelOrder(callback, this.#queue[0]);
+    }
+  }
+
+  // Traverse the tree in depth-first order
+  // run callback function with Node
+  inorder(callback = this.#addToNodesArray, node = this.#root) {
+    // Left Subtree
+    if (node.leftChild !== null) {
+      this.inorder(callback, node.leftChild);
+    }
+
+    // root
+    callback.call(this, node);
+
+    // Right Subtree
+    if (node.rightChild !== null) {
+      this.inorder(callback, node.rightChild);
+    }
+
+    if(callback === this.#addToNodesArray && node === this.#root){
+        const arrayCopy = [...this.#nodesArray]
+        this.#nodesArray = []
+        return arrayCopy
+    }
+
+    return;
+
+  }
+
+  preorder(callback = this.#addToNodesArray, node = this.#root) {
+    // root
+    callback.call(this, node);
+
+    // Left Subtree
+    if (node.leftChild !== null) {
+      this.preorder(callback, node.leftChild);
+    }
+
+    // Right Subtree
+    if (node.rightChild !== null) {
+      this.preorder(callback, node.rightChild);
+    }
+
+    if(callback === this.#addToNodesArray && node === this.#root){
+        const arrayCopy = [...this.#nodesArray]
+        this.#nodesArray = []
+        return arrayCopy
+    }
+
+    return;
+  }
+
+  postorder(callback = this.#addToNodesArray, node = this.#root) {
+    // Left Subtree
+    if (node.leftChild !== null) {
+      this.postorder(callback, node.leftChild);
+    }
+
+    // Right Subtree
+    if (node.rightChild !== null) {
+      this.postorder(callback, node.rightChild);
+    }
+
+    // root
+    callback.call(this, node);
+
+    if(callback === this.#addToNodesArray && node === this.#root){
+        const arrayCopy = [...this.#nodesArray]
+        this.#nodesArray = []
+        return arrayCopy
+    }
+
+    return;
+  }
+
+  #findHeight(node){
+    if(node.leftChild === null && node.rightChild === null){
+        return 0
+    }
+    let depthLeft = 0;
+    if(node.leftChild !== null){
+    depthLeft = 1 + this.#findHeight(node.leftChild)
+    }
+    let depthRight = 0;
+    if(node.rightChild !== null){
+    depthRight = 1 + this.#findHeight(node.rightChild)
+    }
+
+    return Math.max(depthLeft, depthRight)
+  }
+  #findDepth(node) {
+    const value = node.value;
+    let currentNode = this.#root;
+    let count = 0;
+    while (currentNode.value !== value) {
+      if (currentNode.rightChild === null && currentNode.leftChild === null) {
+        count = -1
+        break;
+      }
+      if (currentNode.value > value) {
+        currentNode = currentNode.leftChild;
+        count++
+      } else {
+        currentNode = currentNode.rightChild;
+        count++
+      }
+    }
+    return count;
+  }
+
+  // Returns number of edges in longest path from leaf node
+  height(node){
+    const requestedNode = this.find(node);
+    return this.#findHeight(requestedNode);
+  }
+
+  // Returns number of edges in longest path from root
+  depth(node){
+    const requestedNode = this.find(node);
+    return this.#findDepth(requestedNode)
+  }
+
+  // Returns true if the difference between heights of left and right subtrees
+  // are less than 1
+  isBalanced(){
+    const difference = this.#findHeight(this.#root.leftChild) - this.#findHeight(this.#root.rightChild);
+    console.log(difference)
+    return Math.abs(difference) <= 1
+  }
+
+  // 
+  rebalance(){
+    const arrayFromTree = this.inorder();
+    this.buildTree(arrayFromTree)
+    return this.#root
   }
 
   get root() {
@@ -208,11 +384,35 @@ const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 
 // tree.buildTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 
-tree.insert(6);
+// tree.insert(6);
+
+// tree.prettyPrint();
+// console.log("");
+
+// tree.delete(23);
+
+// tree.prettyPrint();
+
+// tree.levelOrder();
+
+// tree.preorder(console.log);
+// tree.inorder(console.log);
+// tree.postorder(console.log);
+
+// console.log(tree.height(67));
+
+// console.log(tree.depth(6))
+
+// console.log(tree.isBalanced())
+
+[0, 18, -10, -15, -16, -17, 564, 800, 1000].forEach(num => tree.insert(num))
 
 tree.prettyPrint();
-console.log("");
 
-tree.delete(23);
+console.log(tree.isBalanced()) // Should say false
+
+tree.rebalance()
 
 tree.prettyPrint();
+
+console.log(tree.isBalanced())
